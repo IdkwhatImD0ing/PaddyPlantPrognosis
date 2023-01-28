2baa521ac28d0214b98b81958ffb3d1f7ada8a5f
Trique Nguyen <triquenguyen2702@gmail.com>
2023-01-27 22:27:52 -0800

    Doing About

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import hamburgerMenu from "../assets/menu.png";
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
       <nav className="bg-white h-[5rem] flex items-center justify-center px-10 shadow-lg">
         <div className="flex justify-between items-center h-full w-[100rem]">
           <div className="w-[20rem] flex justify-between">
@@ -19,14 +18,6 @@ const Navbar = () => {
               <Link to="/diagnose">Diagnose</Link>  
             </button>
           </div>
-=======
-    <nav className="bg-white text-[#2c302e] text-[15px] font-semibold h-[5rem] flex items-center justify-center px-10 shadow-sm">
-      <div className="flex justify-between items-center h-full w-[100rem]">
-        <div className="w-[18rem] md:flex hidden justify-between">
-          <Link to="/diseases" className="transition duration-100 ease-in-out hover:text-[#727c77]">Diseases</Link>
-          <Link to="/resources" className="transition duration-100 ease-in-out hover:text-[#727c77]">Resources</Link>
-          <Link to="/about" className="transition duration-100 ease-in-out hover:text-[#727c77]">About</Link>
->>>>>>> 877c712e29f17e2c6ab92a99f2ddf170cc844198
         </div>
         <div className="md:hidden cursor-pointer">
           <img src={hamburgerMenu} alt="hamburgerMenu" className="w-8" />
@@ -39,7 +30,6 @@ const Navbar = () => {
             <Link to="/diagnose" className="">Diagnose</Link>  
           </button>
         </div>
-      </div>
     </nav>
       
   )
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -21,9 +21,12 @@ const About = () => {
           <p className='pt-2 pb-8'>Paddy Plant Prognosis assists rice farmers with A.I. technology to detect diseases in their paddies, brings scientific information to light, and helps them with solutions from experts.</p>         
           <h2 className='text-[24px]'>Meet Our Contributors</h2>
           <p className='pt-2'>We are thriving students with passionate in developing tools to help the world!</p>
-          {contacts.map((contact) => {
-            <Contributor photo={contact.photo} name={contact.name} linkedin={contact.linkedin} github={contact.github} info={contact.info} gmail={contact.gmail} />
-          })}
+          <div>
+            {contacts.map(contact => {
+              <Contributor photo={contact.photo} name={contact.name} linkedin={contact.linkedin} github={contact.github} info={contact.info} gmail={contact.gmail} />
+            })}
+          </div>
+          
         </div>
     </div>
   )

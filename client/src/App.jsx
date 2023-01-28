828243e8146a1e6d72d364347314b4af2c3b0ab9
Trique Nguyen <triquenguyen2702@gmail.com>
2023-01-27 20:09:22 -0800

    Doing About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages'
 import { Navbar } from './components'
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <About />
   );
 }
 

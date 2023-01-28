a58ad9474edff10ec7238ad19a3e5de2c8f3fcd2
Simon Quach <simonquach04@gmail.com>
2023-01-27 19:13:28 -0800

    front end setup with vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages'
-
+import { Navbar } from './components'
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
       </Routes>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-red-500 h-[5rem] flex items-center justify-center">
+      <div className="bg-green-500 flex items-center h-full w-[70rem]">
+        <div>
+          a
+        </div>
+        <div>
+          logo
+        </div>
+        <div>
+          <button className="bg-[#9ae19f] px-4 py-2 rounded-lg">Diagnose</button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

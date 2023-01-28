a21110848e2a27a7564f87cd87bf01215e32ea70
Bill Zhang <billzhangsc@gmail.com>
2023-01-27 23:40:12 -0800

    Added model code

---
+++ server/infer.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from keras.applications.mobilenet_v2 import preprocess_input
+
+class Inferrer:
+
+    def __init__(self):
+        self.saved_path = 'model'
+        self.model = tf.saved_model.load(self.saved_path)
+
+        self.predict = self.model.signatures['serving_default']
+
+    def infer(self, image=None):
+        # Preprocesses the image
+        image = preprocess_input(image)
+        # Converts the image to a tensor
+        image = tf.convert_to_tensor(image, dtype=tf.float32)
+        
+        # Gets the prediction)
+        prediction = self.predict(image)['dense_8']
+        # Gets the prediction
+        prediction = prediction.numpy()
+        # Get highest element in prediction
+        percentage = prediction.max()
+        # Gets the prediction
+        prediction = prediction.argmax()
+        return [int(prediction), float(percentage)]

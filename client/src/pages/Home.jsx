836be1f94c183451aa5c6fdce4b41ff6bf8c1e59
Simon Quach <simonquach04@gmail.com>
2023-01-28 00:52:06 -0800

    working on diagnose feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div className="">
-      Home
+    <div className="flex justify-center w-full">
+      <div className="bg-green-200 w-[1440px] h-[555px]">
+        video
+      </div>
     </div>
   )
 }

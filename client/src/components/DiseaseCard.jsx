3079e6bce5d12853ee46041122003389932b7f0f
Trique Nguyen <triquenguyen2702@gmail.com>
2023-01-28 23:46:35 -0800

    done diseases page

--- client/src/components/DiseaseCard.jsx
+++ client/src/components/DiseaseCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const DiseaseCard = ({ name, description }) => {
+const DiseaseCard = ({ photo, name, description }) => {
   return (
-    <div>
-        <h2>{name}</h2>
-        <p>{description}</p>
+    <div className="md:w-[40rem] w-[100%] h-[10rem] flex bg-slate-100 rounded-xl shadow-md">
+        <img className="w-[10rem] rounded-xl object-cover" src={photo} alt="image"/>
+        <div className="flex flex-col justify-between px-6 py-3 text-left">
+            <figcaption>
+                <div className="text-left font-semibold">
+                    {name}
+                </div>
+                <div className="">
+                    {description}
+                </div>
+            </figcaption>
+        </div>
     </div>
   )
 }

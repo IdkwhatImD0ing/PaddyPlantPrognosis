d2b45d7b154d809d00cee69255e1143720e9250b
Simon Quach <simonquach04@gmail.com>
2023-01-28 22:18:46 -0800

    Refined Individual Disease Pages

--- client/src/pages/Disease.jsx
+++ client/src/pages/Disease.jsx
@@ -1,64 +1,56 @@
-import React from 'react';
 import {useParams} from 'react-router';
 import Data from './data.json';
-import {Box, Stack, Typography, List, ListItem} from '@mui/material';
 
 export default function Disease() {
   const {id} = useParams();
   const disease = Data[id];
-  return (
-    <Box
-      width="100%"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
 
-        backgroundColor: 'lightgreen',
-        paddingTop: '6rem',
-        paddingBottom: '100px',
-      }}
-    >
-      <Stack
-        spacing={4}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        width="80%"
-      >
-        <Typography variant="h2" align="center">
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col w-[100rem] mt-[10rem] mb-[5rem] px-8 text-[#2c302e]">
+        <h1 className="text-[36px] font-extrabold font-serif">
           {disease.name}
-        </Typography>
-        <Typography variant="h7" align="center">
-          Scientific Name: {disease.scientific_name}
-        </Typography>
-        <img
-          src={localStorage.getItem('userImage')}
-          alt="image"
-          className="w-[20rem] mt-6 rounded-xl"
-        />
-        <Typography variant="h5">Symptoms: </Typography>
-        <Typography variant="h7">{disease.symptoms}</Typography>
-        <Typography variant="h5">Cause: </Typography>
-        <Typography variant="h7">{disease.cause}</Typography>
-        <Typography variant="h5">How do you prevent this disease?</Typography>
-        <List
-          sx={{
-            listStyleType: 'disc',
-            pl: 2,
-            '& .MuiListItem-root': {
-              display: 'list-item',
-            },
-          }}
-        >
-          {disease.prevention.map((prevention) => (
-            <ListItem>
-              <Typography variant="h7">{prevention}</Typography>
-            </ListItem>
-          ))}
-        </List>
-      </Stack>
-    </Box>
+        </h1>
+
+        {/* Scientific name will not show up for "normal" paddy plants */}
+        {
+          disease.scientific_name ? 
+          <div className="text-[16px]">
+            Scientific Name: <span className="font-bold text-[#537a5a]">{disease.scientific_name}</span>
+          </div>
+          :
+          <></>
+        }
+
+        <img src={`/sample-paddies/${id}.jpg`} alt={`${id}.jpg`} className="w-[20rem] mt-6 rounded-xl" />
+
+        <div className="flex flex-col justify-center max-w-[50rem]">
+          <div className="mt-[2rem]">
+            <p className='text-[18px] text-[#43b348] font-medium'>What happens?</p>
+            <div className="text-[32px] font-bold font-serif">Symptoms: </div>
+            <div className="text-[#666e75] mt-[1rem]">{disease.symptoms}</div>
+          </div>
+
+          <div className="mt-[2rem]">
+            <p className='text-[18px] text-[#43b348] font-medium'>What are the causes?</p>
+            <div className="text-[32px] font-bold font-serif">Cause: </div>
+            <div className="text-[#666e75] mt-[1rem]">{disease.cause}</div>
+          </div>
+          
+          <div className="mt-[2rem]">
+            <p className='text-[18px] text-[#43b348] font-medium'>How do you prevent this disease?</p>
+            <div className="text-[32px] font-bold font-serif">Prevention:</div>
+            <ul className="ml-[2rem] mt-[1rem]">
+              {disease.prevention.map((prevention, index) => (
+                <li key={index} className="text-[#666e75] list-disc">
+                  <div>{prevention}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   );
 }

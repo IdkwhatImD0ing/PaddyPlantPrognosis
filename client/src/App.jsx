def01ed7f1b118bb3296b7bcf6f47c4448775f19
Simon Quach <simonquach04@gmail.com>
2023-01-29 01:24:46 -0800

    Added animations for about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route}  from 'react-router-dom';
 
 import {Home, Diseases, Resources, About, Diagnose, Disease } from './pages';
 import { Navbar, Footer } from './components';
